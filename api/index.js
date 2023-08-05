d5480ab80633a848411b9d3b1d662421afaadcb6
Sahand Ghavidel <sahand.gha@gmail.com>
2023-08-05 11:35:58 +0700

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
