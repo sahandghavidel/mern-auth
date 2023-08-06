f79e79b553395f4f0a8addbb215bd27da4f6e754
Sahand Ghavidel <sahand.gha@gmail.com>
2023-08-06 09:08:00 +0700

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+}, {timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
