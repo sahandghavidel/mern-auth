ce225920e16ca356808e5e075e32acc1e9b00d7e
Sahand Ghavidel <sahand.gha@gmail.com>
2023-08-06 10:07:29 +0700

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -15,8 +16,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
 
 app.use('/api/user', userRoutes);
+app.use("/api/auth", authRoutes)
